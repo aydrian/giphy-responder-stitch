7018fa3b9896ad18d97467034c97b792089c91c9
Aydrian Howard <aydrian@gmail.com>
2018-04-14 17:29:49 -0400

    pulling from Stitch

--- webui/src/App.js
+++ webui/src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
+import { StitchClientFactory } from 'mongodb-stitch';
 import logo from './logo.svg';
 import './App.css';
 
+let stitchClientPromise = StitchClientFactory.create('giphyresponder-mspiu');
+
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      responses: []
+    }
+  }
+
+  componentWillMount () {
+    stitchClientPromise.then(stitchClient => stitchClient.login()
+      .then(authedId => {
+        console.log(`logged in as:  + ${authedId}`)
+        let db = stitchClient.service('mongodb', 'mongodb-atlas').db('porg');
+        return db.collection('responses').find({}).execute()
+      }).then(responses => {
+        console.log(responses)
+        this.setState({ responses });
+      })
+      .catch(e => console.log('error: ', e))
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Giphy Responder</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          { this.state.responses.map(response => {
+            return (
+              <li key={response._id}>{response.search}</li>
+            );
+          })}
+        </ul>
       </div>
     );
   }
